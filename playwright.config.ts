d74eae1908db92431f0d876b4dcb59f94e58ba1d
RAINEY5900 <topbs809@gmail.com>
2026-06-27 15:08:26 +0500

    fix playwright config testMatch for .pl.tsx files

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  testMatch: '**/*.pl.tsx',
   use: {
     baseURL: 'http://localhost:4000'
   }
